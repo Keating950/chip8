b47ae44cb7b2f687f8119aeae7d965813107426a
Keating950 <keating950@gmail.com>
2020-03-16 21:23:00 -0400

    using non reentrant ht to avoid SIGFPE. Adding superstructure for timer handling

--- main.c
+++ main.c
@@ -6,24 +6,27 @@
 #include <SDL_video.h>
 #include <stdio.h>
 #include <search.h>
+#include <time.h>
+
+#ifdef __MACH__
+timing_mach_init();
+#endif
+
 #define handle_error(msg)                                                      \
 	do {                                                                   \
 		perror(msg);                                                   \
 		exit(EXIT_FAILURE);                                            \
 	} while (0)
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wint-to-pointer-cast"
-struct hsearch_data *create_key_htab()
+void create_key_htab()
 {
 	/* FLAG: Narrowing conversion!
 	  SDLK macros are ints. However, with my
 	  control scheme, they should not overflow. */
 	ENTRY e, *ep;
-	int sdl_keys[0x10]
+	char sdl_keys[0x10]
 		= { SDLK_y, SDLK_u, SDLK_i, SDLK_o, SDLK_h,	SDLK_j,
 		    SDLK_k, SDLK_l, SDLK_n, SDLK_m, SDLK_COMMA, SDLK_PERIOD };
-	struct hsearch_data *htab = calloc(23, sizeof(struct hsearch_data));
 
 	if (hcreate(23) == 0) { // zero on error -- it's weird
 		perror("Error creating keyboard hashtable");
@@ -37,58 +40,50 @@ struct hsearch_data *create_key_htab()
 		*chip8_key = i;
 		e.key = sdl_key;
 		e.data = chip8_key;
-		hsearch_r(e, ENTER, &ep, htab);
+		ep = hsearch(e, ENTER);
 		if (!(ep))
 			handle_error("Couldn't add key to hashtable");
 	}
-	return htab;
 }
-#pragma clang diagnostic pop
 
-void main_loop(chip8_vm vm)
+void main_loop(chip8_vm *vm)
 {
 	SDL_Window *win;
 	SDL_Surface *surf;
-	SDL_Event e;
-	init_window(&win, &surf);
+	SDL_Event event;
+	struct ENTRY *ht_entry = { 0, 0 };
+	struct timespec delay_ts;
+	struct timespec sound_ts;
+	struct timespec current_ts;
 
-	while (SDL_PollEvent(&e)) {
-		if (e.type == SDL_QUIT)
+	clock_gettime(CLOCK_MONOTONIC, &delay_ts);
+	clock_gettime(CLOCK_MONOTONIC, &sound_ts);
+	init_window(&win, &surf);
+	while (SDL_PollEvent(&event)) {
+		if (event.type == SDL_QUIT)
 			break;
-		else if (e.type == SDL_KEYDOWN) {
-			switch (e.key.keysym.sym) {
-			case SDLK_y:
-				break;
-			case SDLK_u:
-				break;
-			case SDLK_i:
-				break;
-			case SDLK_o:
-				break;
-			case SDLK_p:
-				break;
-			case SDLK_h:
-				break;
-			case SDLK_j:
-				break;
-			case SDLK_k:
-				break;
-			case SDLK_l:
-				break;
-			case SDLK_n:
-				break;
-			case SDLK_m:
-				break;
-			case SDLK_COMMA:
-				break;
-			case SDLK_PERIOD:
-				break;
-			default:
-				break;
-			}
+		else if (event.type == SDL_KEYDOWN || event.type == SDL_KEYUP) {
+			ht_entry.key = event.key.keysym.sym;
+			hsearch(ht_entry, FIND);
+			vm->keyboard[ht_entry.data]
+				= !(vm->keyboard[ht_entry.data]);
+		}
+		vm_cycle(vm);
+		if (vm->draw_flag) {
+			// draw stuff
+		}
+		clock_gettime(CLOCK_MONOTONIC, &current_ts);
+		if (current_ts.tv_sec - delay_ts.tv_sec
+		    >= vm->delay_timer / 60) {
+			// delay stuff
+			vm->delay_timer = 0;
+		}
+		if (current_ts.tv_sec - sound_ts.tv_sec
+		    >= vm->sound_timer / 60) {
+			// beep
+			vm->sound_timer = 0;
 		}
 	}
-
 	SDL_DestroyWindow(win);
 	SDL_Quit();
 }
@@ -102,12 +97,12 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
-	struct hsearch_data *htab = create_key_htab();
+	create_key_htab();
 	chip8_vm vm = initialize_chip8();
 	load_rom(argv[1], &vm);
 	vm_cycle(&vm);
-	//    SDL_Window *win;
-	//    SDL_Surface *surf;
-	//    init_window(&win, &surf);
+	//	SDL_Window *win;
+	//	SDL_Surface *surf;
+	//	init_window(&win, &surf);
 	return 0;
 }
