8da625dd4541a87264c460ebf34fd417fa305b11
Keating950 <keating.reid@protonmail.com>
2020-11-13 11:05:16 -0500

    refactoring key handling code

--- main.c
+++ main.c
@@ -26,13 +26,12 @@ static SDL_Window *win = NULL;
 static SDL_Renderer *renderer = NULL;
 static SDL_Texture *vm_texture = NULL;
 
-// argc, argv format required by SDL
 int main(int argc, char **argv)
 {
 	if (argc < 2) {
 		fputs("Wrong number of arguments.\n"
-			  "Usage:\n\tchip8 [path to rom]\n",
-			  stderr);
+		      "Usage:\n\tchip8 [path to rom]\n",
+		      stderr);
 		exit(EXIT_FAILURE);
 	}
 	sdl_init();
@@ -63,7 +62,8 @@ void main_loop(chip8_vm *vm)
 			case SDL_KEYDOWN:
 				if (event.key.keysym.scancode == PAUSE_KEY) {
 					ops_since_draw = 0;
-					clock_gettime(CLOCK_MONOTONIC, &frame_start);
+					clock_gettime(CLOCK_MONOTONIC,
+						      &frame_start);
 					paused = !paused;
 					break;
 				} else
@@ -88,9 +88,10 @@ void main_loop(chip8_vm *vm)
 			vm->sound_timer = ZERO_FLOOR(vm->sound_timer - 1);
 			draw_screen(vm);
 			clock_gettime(CLOCK_MONOTONIC, &frame_end);
-			delta =
-				ZERO_FLOOR((TIMER_HZ_NS - difftime_ns(&frame_start, &frame_end)))
-				/ 1000;
+			delta = ZERO_FLOOR((TIMER_HZ_NS -
+					    difftime_ns(&frame_start,
+							&frame_end))) /
+				1000;
 			if (delta)
 				usleep(delta);
 		}
@@ -116,7 +117,7 @@ void draw_screen(const chip8_vm *vm)
 	if (SDL_RenderClear(renderer))
 		DIE(SDL_GetError());
 	if (SDL_UpdateTexture(vm_texture, &src, (void *)vm->screen,
-						  0x40 * sizeof(uint32_t)))
+			      0x40 * sizeof(uint32_t)))
 		DIE(SDL_GetError());
 	SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
 	SDL_RenderCopy(renderer, vm_texture, &src, &dest);
@@ -127,14 +128,14 @@ void sdl_init(void)
 {
 	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS))
 		DIE("Could not initialize SDL");
-	SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN,
-								&win, &renderer);
+	SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT,
+				    SDL_WINDOW_SHOWN, &win, &renderer);
 	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, 0);
 	if (!(win) || !(renderer))
 		DIE(SDL_GetError());
 	vm_texture = SDL_CreateTexture(renderer, SDL_GetWindowPixelFormat(win),
-								   SDL_TEXTUREACCESS_STREAMING, SCREEN_WIDTH,
-								   SCREEN_HEIGHT);
+				       SDL_TEXTUREACCESS_STREAMING,
+				       SCREEN_WIDTH, SCREEN_HEIGHT);
 	if (!vm_texture)
 		DIE(SDL_GetError());
 	if (SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF))
@@ -146,21 +147,13 @@ void sdl_init(void)
 
 void update_vm_keyboard(chip8_vm *vm)
 {
+	static const SDL_Scancode keys[] = {
+		CHIP8_KEY_0, CHIP8_KEY_1, CHIP8_KEY_2, CHIP8_KEY_3,
+		CHIP8_KEY_4, CHIP8_KEY_5, CHIP8_KEY_6, CHIP8_KEY_7,
+		CHIP8_KEY_8, CHIP8_KEY_9, CHIP8_KEY_A, CHIP8_KEY_B,
+		CHIP8_KEY_C, CHIP8_KEY_D, CHIP8_KEY_E, CHIP8_KEY_F,
+	};
 	const uint8_t *kbd_state = SDL_GetKeyboardState(NULL);
-	vm->keyboard[0x0] = kbd_state[CHIP8_KEY_0];
-	vm->keyboard[0x1] = kbd_state[CHIP8_KEY_1];
-	vm->keyboard[0x2] = kbd_state[CHIP8_KEY_2];
-	vm->keyboard[0x3] = kbd_state[CHIP8_KEY_3];
-	vm->keyboard[0x4] = kbd_state[CHIP8_KEY_4];
-	vm->keyboard[0x5] = kbd_state[CHIP8_KEY_5];
-	vm->keyboard[0x6] = kbd_state[CHIP8_KEY_6];
-	vm->keyboard[0x7] = kbd_state[CHIP8_KEY_7];
-	vm->keyboard[0x8] = kbd_state[CHIP8_KEY_8];
-	vm->keyboard[0x9] = kbd_state[CHIP8_KEY_9];
-	vm->keyboard[0xA] = kbd_state[CHIP8_KEY_A];
-	vm->keyboard[0xB] = kbd_state[CHIP8_KEY_B];
-	vm->keyboard[0xC] = kbd_state[CHIP8_KEY_C];
-	vm->keyboard[0xD] = kbd_state[CHIP8_KEY_D];
-	vm->keyboard[0xE] = kbd_state[CHIP8_KEY_E];
-	vm->keyboard[0xF] = kbd_state[CHIP8_KEY_F];
+	for (int i = 0x0; i < 0xF; i++)
+		vm->keyboard[i] = kbd_state[keys[i]];
 }
